96716f055335689a2cfcbf4d58dea7dd4e8ac11e
Søren Engholm Gjesing <SGJ@kmd.dk>
2020-08-31 13:39:02 +0200

    Add project files.

---
+++ LogOn/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogOn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public void RunQuery(ODBC odbc, string _sqlQ)
+        {
+            string ConectStreng;
+            ConectStreng = "DSN=BZDSNT;UID=Z6SGJ;Pwd=xxxxxx";
+
+            using (var conn = new OdbcConnection(ConectStreng))
+            {
+                try
+                {
+                    OdbcCommand cmd = new OdbcCommand(_sqlQ, conn);
+                    cmd.CommandTimeout = 1000 * 60; // 5 minutter
+                    conn.Open();
+                    var schema = conn.GetSchema();
+                    var datasource = conn.DataSource;
+                    using (OdbcDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            //SqlResult res = new SqlResult();
+                            //result.Add(res);
+                            //Kolloner  col = new Kolloner();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                string value;
+                                value = reader.GetValue(i).ToString();
+
+                            }
+                        }
+                    }
+                }
+                catch (OdbcException ex)
+                {
+                    if (ex.Message.Contains("USERNAME AND"))
+                    {
+                        MessageBox.Show("Formentlig password fejl. Der indhentes nyt ved næste SQL opslag." + "\n" + ex.Message);
+                    }
+                    else
+                        MessageBox.Show(ex.Message);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                finally
+                {
+                    conn.Close();
+                }
+
+            }
+
+        }
+
+        private void brtStart_Click_1(object sender, RoutedEventArgs e)
+        {
+            string sqlQ = "SELECT ID_ORG_ADM_ENHED"
+                        + " FROM UDV1.SS61000T"
+                        + " FETCH FIRST 10 ROWS ONLY";
+            var _Odbc = new ODBC() { OdbcNavn = "BZDSNT", };
+            RunQuery(_Odbc, sqlQ);
+        }
+    }
+}
---
+++ LogOn/ODBC.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace LogOn
+{
+    public class ODBC
+    {
+        public string OdbcNavn { get; set; }
+        public string OdbcType { get; set; }
+        public string LastCreator { get; set; }
+        public string LastUser { get; set; }
+        public string LastCryptPw { get; set; }
+        public string LastPWChange { get; set; }
+
+
+        public ODBC(string name)
+        {
+            OdbcNavn = name;
+        }
+        public ODBC()
+        {
+
+        }
+
+        public void gemSetting(ODBC _Odbc, string fRuninfo)
+        {
+            var path = fRuninfo.Trim();
+            System.Xml.Serialization.XmlSerializer writer =
+                             new System.Xml.Serialization.XmlSerializer(typeof(ODBC));
+            System.IO.FileStream file = System.IO.File.Create(path);
+            writer.Serialize(file, _Odbc);
+            file.Close();
+        }
+        public ODBC HentSetting(string fnavn)
+        {
+            var path = fnavn;
+            ODBC s1 = new ODBC();
+            //ODBC s1 = this;
+            if (!File.Exists(path))
+            {
+                return (s1);
+                //throw new System.InvalidOperationException("System.xml cannot be found");
+            }
+            System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(ODBC));
+
+            StreamReader file = new System.IO.StreamReader(path);
+            s1 = (ODBC)reader.Deserialize(file);
+            file.Close();
+            return (s1);
+        }
+
+    }
+}
