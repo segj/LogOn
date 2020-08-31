32ca862afaef86f89454acfaf90ddd4957ecc9c2
Søren Engholm Gjesing <SGJ@kmd.dk>
2020-08-31 14:34:39 +0200

    iab

--- LogOn/MainWindow.xaml.cs
+++ LogOn/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Odbc;
 using System.Linq;
 using System.Text;
@@ -21,9 +22,21 @@ namespace LogOn
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DataTable dt;
         public MainWindow()
         {
             InitializeComponent();
+            dt = new DataTable();
+            dt.Columns.Add("Id_org_adm", typeof(string));
+            dt.Columns.Add("Personnummer", typeof(string));
+            dt.Columns.Add("Kle_UUID", typeof(string));
+            /*dt.Rows.Add(new object[3] { "Mary", 22, 1 });
+            dt.Rows.Add(new object[3] { "Peter", 24, 3 });
+            dt.Rows.Add(new object[3] { "Rose", 17, 1 });
+            dt.Rows.Add(new object[3] { "John", 19, 2 });
+            dt.Rows.Add(new object[3] { "Steven", 20, 1 });
+            dt.Rows.Add(new object[3] { "Tom", 20, 3 });*/
+            dataGrid.ItemsSource = dt.AsDataView();
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -52,11 +65,15 @@ namespace LogOn
                             //SqlResult res = new SqlResult();
                             //result.Add(res);
                             //Kolloner  col = new Kolloner();
+                            string value, value1, value2;
+                            value = reader.GetValue(0).ToString();
+                            value1 = reader.GetValue(1).ToString();
+                            value2 = reader.GetValue(2).ToString();
+                            dt.Rows.Add(new object[3] {value, value1, value2 });
+                            
                             for (int i = 0; i < reader.FieldCount; i++)
                             {
-                                string value;
                                 value = reader.GetValue(i).ToString();
-
                             }
                         }
                     }
@@ -88,10 +105,13 @@ namespace LogOn
         private void brtStart_Click_1(object sender, RoutedEventArgs e)
         {
             string sqlQ = "SELECT ID_ORG_ADM_ENHED"
+                        + ", PERSONNUMMER"
+                        + ", KLE_UUID"
                         + " FROM UDV1.SS61000T"
                         + " FETCH FIRST 10 ROWS ONLY";
             var _Odbc = new ODBC() { OdbcNavn = "BZDSNT", };
             RunQuery(_Odbc, sqlQ);
         }
     }
+    
 }
